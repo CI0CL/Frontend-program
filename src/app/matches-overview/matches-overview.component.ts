5a39fc874be754381f992dc5898ae7cf14219be9
Coco <clemency.commission@gmail.com>
2023-08-23 22:39:19 +0200

    I DID IT OMG WHUT

--- src/app/matches-overview/matches-overview.component.ts
+++ src/app/matches-overview/matches-overview.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+/*import { Component, Input } from '@angular/core';
 import { HeaderTextService } from '../header-text.service';
 import { Match } from '../shared/match';
 
@@ -15,5 +15,31 @@ export class MatchesOverviewComponent {
 
   ngOnInit() {
     this.headerService.setHeaderText('Matches Page');
+  }
+}
+*/
+import { Component, OnInit } from '@angular/core';
+import { Match } from '../shared/match';
+import { MatchService } from '../matches/match/match.service';
+
+@Component({
+  selector: 'ov-matches-overview',
+  templateUrl: './matches-overview.component.html',
+  styleUrls: ['./matches-overview.component.scss']
+})
+export class MatchesOverviewComponent implements OnInit {
+  matches: Match[] = [];
+
+  constructor(private matchService: MatchService) { }
+
+  ngOnInit(): void {
+    this.loadMatches();
+  }
+
+  loadMatches(): void {
+    this.matchService.getMatches().subscribe(
+      data => this.matches = data,
+      error => console.error(error)
+    );
   }
 }
--- src/app/matches/match/match.service.ts
+++ src/app/matches/match/match.service.ts
@@ -1,33 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 import { Match } from 'src/app/shared/match';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class MatchService {
-  private baseUrl = 'http://your-backend-api-url'; // Replace with your actual API URL
+  private baseUrl = 'http://localhost:8080';
 
   constructor(private http: HttpClient) { }
 
   getMatches(): Observable<Match[]> {
     const url = `${this.baseUrl}/api/matches`;
-    return this.http.get<any[]>(url).pipe(
-      catchError(this.handleError)
-    );
-  }
-
-  getMatchById(matchId: number): Observable<Match> {
-    const url = `${this.baseUrl}/api/match/${matchId}`;
-    return this.http.get<any>(url).pipe(
-      catchError(this.handleError)
-    );
-  }
-
-  private handleError(error: any): Observable<never> {
-    console.error('An error occurred:', error);
-    return throwError(error);
+    return this.http.get<Match[]>(url);
   }
 }
